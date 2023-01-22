ed6eef981576b820672d020fb247a79a4b23d12e
Việt An <vietan00892b@gmail.com>
2023-01-22 23:24:03 +0700

    Added error message if connect to DB fails

--- connectDB.js
+++ connectDB.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGO_URI, {}, () => {
+mongoose.connect(process.env.MONGO_URI, {}, (err) => {
+  if (err) console.log('Connecting to database failed!');
   console.log('Connected to database!');
 });
